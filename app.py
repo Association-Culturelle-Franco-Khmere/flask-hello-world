6160a3b853377aa188acba91f12cc9f05dc6022b
SuPeRNoiSH <michel.meas@gmail.com>
2025-02-17 16:26:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Wesh mec!'
+
+from flask import Flask, request, Response
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Wesh mec!'
+@app.route('/webhook', methods=['POST'])
+def respond():
+    print(request.json);
+    return Response(status=200)
